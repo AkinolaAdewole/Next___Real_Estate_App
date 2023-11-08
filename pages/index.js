a99163f6cf6c6de5c0f8e589b968262ab8a9fc85
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-08 19:47:25 +0100

    Estate App Clone

--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,36 @@ import { Flex, Box, Text, Button } from '@chakra-ui/react';
 import Property from '../components/Property';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 
+// This exports the 'Banner' component as a named export from this module.
 export const Banner = ({ purpose, title1, title2, desc1, desc2, buttonText, linkName, imageUrl }) => (
+  // This component accepts several props, which are destructured from the object passed to it.
+
+  // A Flex component is used to create a flexible container for the banner content.
   <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
+    // An Image component is used to display an image with the specified URL, width, and height.
     <Image src={imageUrl} width={500} height={300} />
+
+    // A Box component is used to create a box that contains the text content of the banner.
     <Box p='5'>
+      // Text components are used to display text with specific styles.
+      // 'purpose' is displayed in small gray text with medium font weight.
       <Text color='gray.500' fontSize='sm' fontWeight='medium'>{purpose}</Text>
+
+      // 'title1' and 'title2' are displayed in a larger font size with bold font weight.
       <Text fontSize='3xl' fontWeight='bold'>{title1}<br />{title2}</Text>
+
+      // 'desc1' and 'desc2' are displayed in a large font size with gray text color.
       <Text fontSize='lg' paddingTop='3' paddingBottom='3' color='gray.700'>{desc1}<br />{desc2}</Text>
+
+      // A Button component is used to create a button with specific styling.
       <Button fontSize='xl' bg="blue.300" color="white">
+        // A Link component is used to create a hyperlink.
         <Link href={linkName}><a>{buttonText}</a></Link>
       </Button>
     </Box>
   </Flex>
 );
+
 
 
 const Home = ({ propertiesForSale, propertiesForRent }) => (
