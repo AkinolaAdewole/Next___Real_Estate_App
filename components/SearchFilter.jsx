a23cd79bccda7c56d50fb8ad82016c173f66dfbe
Akinola Adewole <adewoleakinola24@gmail.com>
2023-11-10 21:33:01 +0100

    Estate App Clone

--- components/SearchFilter.jsx
+++ components/SearchFilter.jsx
@@ -3,7 +3,6 @@ import { Flex, Select, Box, Text, Input, Spinner, Icon, Button } from '@chakra-u
 import { useRouter } from 'next/router';
 import { MdCancel } from 'react-icons/md';
 import Image from 'next/image';
-
 import { filterData, getFilterValues } from '../utils/filterData';
 import { baseUrl, fetchApi } from '../utils/fetchApi';
 import noresult from '../assets/images/noresult.svg';
